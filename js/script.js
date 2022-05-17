a88ae66f75bcb565dc88a5a604ccff8fa6fa8a87
fracabu <100199965+fracabu@users.noreply.github.com>
2022-05-17 23:48:48 +0200

    esercizio

---
+++ js/script.js
@@ -0,0 +1,15 @@
+
+// interazioni
+const nomeUtente = prompt("quale è il tuo nome?")
+const cognomeUtente = prompt("quale è il tuo cognome?")
+const colorePreferito = prompt("qual'è il tuo colore preferito?")
+
+
+const spanNomeUtente = document.getElementById ("span-nome-utente");
+
+
+spanNomeUtente.innerHTML = "MarioRossiRosso22"
+
+
+
+
